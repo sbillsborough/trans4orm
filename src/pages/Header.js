c2a9b13c686af176296a316855827e785941a976
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-16 18:12:49 +0100

    added the header and opening times / contact number with a table. Set to flex and added padding. Set colours

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,18 @@
 function Header() {
   return (
     <header>
-      <h1>Hello World!</h1>;
+      <table>
+        <tr>
+          <th>Opening hours: Mon - Fri:</th>
+          <td>05:00- 22:00</td>
+          <th>Sat-Sun:</th>
+          <td>08:00- 18:00</td>
+        </tr>
+        <tr>
+          <th>Call us today:</th>
+          <td>01942 597961</td>
+        </tr>
+      </table>
     </header>
   );
 }
