647af6acc163a94480e654ed01a1b1c8086684d0
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-17 17:19:20 +0100

    navbar styling completed and looking similar to the design

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,23 +1,23 @@
 function NavBar() {
   return (
     <div className="nav-container">
-      <h1 id="logo">T4</h1>
       <nav>
+        <h1 id="logo">T4</h1>
         <a href="#">Home</a>
         <a href="#">About Us</a>
         <a href="#">Our Pt's</a>
         <a href="#">Classes & Timetable</a>
         <a href="#">Blogs</a>
         <a href="#">Contact Us</a>
+        <table>
+          <tr>
+            <th>Contact us today</th>
+            <td>
+              <button>Sign up today</button>
+            </td>
+          </tr>
+        </table>
       </nav>
-      <table>
-        <tr>
-          <th>Contact us today</th>
-          <td>
-            <button>Sign up today</button>
-          </td>
-        </tr>
-      </table>
     </div>
   );
 }
