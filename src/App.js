4a626b75521f6f9f29ae04eed4ef77985532387c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-17 17:39:35 +0100

    added the hero component ready to build out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./pages/Header";
 import NavBar from "./pages/NavBar";
+import Hero from "./pages/Hero";
 
 function App() {
   return (
     <>
       <Header />
       <NavBar />
+      <Hero />
     </>
   );
 }
---
+++ src/pages/Hero.js
@@ -0,0 +1,5 @@
+function Hero() {
+  return <h1>Hello World!</h1>;
+}
+
+export default Hero;
