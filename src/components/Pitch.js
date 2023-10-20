c57f1cfff783e584d4bfe565e90947e9a2adbd30
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-20 16:00:31 +0100

    styled and positioned the cards

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -3,8 +3,41 @@ import sparkles from "../resources/img/sparkles.png";
 function Pitch() {
   return (
     <div className="pitch-container">
-      <h2>What sets us apart from the rest?</h2>
-      <div className="pitch-cards"></div>
+      <h3>What sets us apart from the rest?</h3>
+      <div className="pitch-cards-container">
+        <div className="pitch-card">
+          <img src={sparkles} className="sparkles" alt="Sparkle icon" />
+          <h4>We’re Experienced</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do enum
+            vulpur.
+          </p>
+        </div>
+        <div className="pitch-card">
+          <img src={sparkles} className="sparkles" alt="Sparkle icon" />
+          <h4>We’re Responsive</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do enum
+            vulpur.
+          </p>
+        </div>
+        <div className="pitch-card">
+          <img src={sparkles} className="sparkles" alt="Sparkle icon" />
+          <h4>We’re Consistent</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do enum
+            vulpur.
+          </p>
+        </div>
+        <div className="pitch-card">
+          <img src={sparkles} className="sparkles" alt="Sparkle icon" />
+          <h4>We’re Quantifiable</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do enum
+            vulpur.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
