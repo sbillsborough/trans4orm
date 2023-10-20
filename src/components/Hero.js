ec696e34db4811ef0ffd20f79e1cbab7dba81313
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-20 14:41:34 +0100

    reorganised the home.js html into separate components, added necessary imports and exports

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="image-text">
+        <p>Achieve Your Fitness Goal.</p>
+        <p>TRAIN IN YOUR OWN TIME, IN WIGAN !</p>
+        <p>
+          We all have our own reasons for going to the gym, however we all have
+          one thing is common, we want to improve and be the best version of
+          ourselves. TRANS4ORM's main purpose is to help people transform
+          physically and mentally in a professional, friendly, and safe
+          environment.
+        </p>
+        <button>Sign up today</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,3 @@
-import logo from "../resources/img/logo.png";
-
 function Home() {
   return (
     <>
@@ -17,67 +15,6 @@ function Home() {
           </tr>
         </table>
       </header>
-      <div className="nav-container">
-        <nav>
-          <img src={logo} className="logo" alt="T4 Logo" />
-          <ul>
-            <li>
-              <a href="home">Home</a>
-            </li>
-            <li>
-              <a href="about">About Us</a>
-            </li>
-            <li>
-              <a href="ourpt">Our Pt's</a>
-            </li>
-            <li>
-              <a href="classes">Classes & Timetable</a>
-            </li>
-            <li>
-              <a href="blogs">Blogs</a>
-            </li>
-            <li>
-              <a href="contact">Contact Us</a>
-            </li>
-          </ul>
-          <table>
-            <tr>
-              <th>Contact us today</th>
-              <td>
-                <button>Sign up today</button>
-              </td>
-            </tr>
-          </table>
-        </nav>
-      </div>
-      <div className="hero">
-        <div className="image-text">
-          <p>Achieve Your Fitness Goal.</p>
-          <p>TRAIN IN YOUR OWN TIME, IN WIGAN !</p>
-          <p>
-            We all have our own reasons for going to the gym, however we all
-            have one thing is common, we want to improve and be the best version
-            of ourselves. TRANS4ORM's main purpose is to help people transform
-            physically and mentally in a professional, friendly, and safe
-            environment.
-          </p>
-          <button>Sign up today</button>
-        </div>
-      </div>
-
-      <section className="intro">
-        <h5>Achieve Your Fitness Goal.</h5>
-        <p>Fitness Should be accessible to everyone.</p>
-        <p>We are dedicated to helping you achieve your fitness goals.</p>
-        <p>
-          We all have our own reasons for going to the gym, however we all have
-          one thing is common, we want to improve and be the best version of
-          ourselves.
-        </p>
-        <button>Sign up today</button>
-      </section>
-      <div className="made-img"></div>
-      <div className="pitch"></div>
     </>
   );
 }
