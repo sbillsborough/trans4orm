a1b2180b0fb2fded5895e72b5246f2e3f613424e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-20 15:22:53 +0100

    added a pitch-container and a pitch cards container. Imported the sparkles logo

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -1,5 +1,12 @@
+import sparkles from "../resources/img/sparkles.png";
+
 function Pitch() {
-  return <div className="pitch"></div>;
+  return (
+    <div className="pitch-container">
+      <h2>What sets us apart from the rest?</h2>
+      <div className="pitch-cards"></div>
+    </div>
+  );
 }
 
 export default Pitch;
