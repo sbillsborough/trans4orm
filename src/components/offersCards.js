a986ec350e604a5c6aad04e871bd49051bfab35d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-23 11:49:03 +0100

    added last two offers cards and positioned, set container to flex so they're all in a line

--- src/components/offersCards.js
+++ src/components/offersCards.js
@@ -2,6 +2,8 @@ function OffersCards() {
   return (
     <div className="offers-cards-container">
       <div className="offers-cards-40"></div>
+      <div className="offers-cards-38"></div>
+      <div className="offers-cards-39"></div>
     </div>
   );
 }
