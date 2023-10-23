ecc5c97d1a70dedf9dceedbbc47b667c8a585156
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-23 13:41:41 +0100

    added and positioned card heading and popular button, styled the border of the popular button with linear gradient

--- src/components/offersCards.js
+++ src/components/offersCards.js
@@ -2,7 +2,12 @@ function OffersCards() {
   return (
     <>
       <div className="offers-cards-container">
-        <div className="offers-cards-40"></div>
+        <div className="offers-cards-40">
+          <div className="monthly-offer-heading-container">
+            <h4>MONTHLY PACKAGE</h4>
+            <button className="popular">Popular</button>
+          </div>
+        </div>
         <div className="offers-cards-38"></div>
         <div className="offers-cards-39"></div>
       </div>
