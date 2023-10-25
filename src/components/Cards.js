f676b0499e988249ca520234bcfdb97afca030b1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-25 16:34:40 +0100

    added the text to the cards, updated the owntime component text to match the design. Imported the Satoshi font through the cdn

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,10 +2,26 @@ function Cards() {
   return (
     <div className="cards">
       <div className="rectangle-left">
+        <div className="rectangle-text-container">
+          <h4>We are dedicated to helping you achieve your.</h4>
+          <p>
+            We all have our own reasons for going to the gym, however we all
+            have one thing in common, we want to improve and be the best version
+            of ourselves.
+          </p>
+        </div>
         <div className="left-front-card"></div>
         <div className="left-rear-card"></div>
       </div>
       <div className="rectangle-right">
+        <div className="rectangle-text-container">
+          <h4>We are dedicated to helping you achieve your.</h4>
+          <p>
+            We all have our own reasons for going to the gym, however we all
+            have one thing in common, we want to improve and be the best version
+            of ourselves.
+          </p>
+        </div>
         <div className="right-front-card"></div>
         <div className="right-rear-card"></div>
       </div>
