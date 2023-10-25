89f089d30c57fe88916b4c0a82ecef4d859861f6
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-25 17:21:25 +0100

    added the frequently asked questions component, positioned and styled

---
+++ src/components/FrequentlyAsked.js
@@ -0,0 +1,14 @@
+function FrequentlyAsked() {
+  return (
+    <div className="frequently-asked">
+      <h5>FAQ's</h5>
+      <span>frequently asked questions</span>
+      <span>
+        We all have our own reasons for going to the gym, however we all have
+        one thing is common, we want to improve
+      </span>
+    </div>
+  );
+}
+
+export default FrequentlyAsked;
