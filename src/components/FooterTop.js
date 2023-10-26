6e5cbd966844b3b9bd4ed48c9530999fb8ffa8a5
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-26 14:39:28 +0100

    bottom footer section content added and positioned/styled

--- src/components/FooterTop.js
+++ src/components/FooterTop.js
@@ -3,7 +3,14 @@ function FooterTop() {
     <div className="footer-container">
       <footer>
         <div className="top"></div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <div className="bottom-text">
+            <span>made better.</span>
+            <span>made different.</span>
+            <span>made for everyone.</span>
+          </div>
+          <button>Sign up today</button>
+        </div>
       </footer>
     </div>
   );
