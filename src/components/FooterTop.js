990db31c8c21a944442df94f9fe6d47e36d906cd
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-10-26 14:06:45 +0100

    footer backgrounds are added and positioned/styled

--- src/components/FooterTop.js
+++ src/components/FooterTop.js
@@ -1,5 +1,12 @@
 function FooterTop() {
-  return <div className="footer-rectangle"></div>;
+  return (
+    <div className="footer-container">
+      <footer>
+        <div className="top"></div>
+        <div className="bottom"></div>
+      </footer>
+    </div>
+  );
 }
 
 export default FooterTop;
